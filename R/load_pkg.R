c8e16afeb6c09ba7aa6925f80b4e9fdccd35bec7
Junjie@MacStudio.local <junjie@junjielei.com>
2024-02-21 19:21:11 -0600

    Stop tracking preprocess.Rproj and update .gitignore

--- R/load_pkg.R
+++ R/load_pkg.R
@@ -1,9 +1,19 @@
+##################################################################
+##                      Creator : Junjie L                      ##
+##                  Creation Date : 03/21/2024                  ##
+##               Title : R pkg data preprocessing               ##
+##################################################################
 
+# Package Loading Sequences;
+## ---------------------------------------------------------------
 load_pkg <- function(){
+  library(readxl)
+  library(openxlsx)
   library(dplyr)
+  library(stringr)
   library(lubridate)
   library(ggplot2)
-  library(ggthemes)
+  library(tidyr)
 }
 
 
